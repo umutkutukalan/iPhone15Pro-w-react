e19bcb34c8b0ac393722b62b07254d4d8a2cb042
Umut Kütükalan <118997325+umutkutukalan@users.noreply.github.com>
2024-07-30 12:10:02 +0300

    tailwind & snippets

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Highlight from "./components/Highlight";
+
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Home />
+      <Highlight />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Highlight.jsx
@@ -0,0 +1,7 @@
+const Highlight = () => {
+  return(
+    <div>Highlight</div> 
+  );
+};
+
+export default Highlight;
