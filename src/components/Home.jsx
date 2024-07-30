6e2024a20eebcd63c832d362794eeca1a5e2ae4b
Umut Kütükalan <118997325+umutkutukalan@users.noreply.github.com>
2024-07-30 14:45:32 +0300

    3-install-GSAP

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  useGSAP(() => {
+    gsap.to('#hero', {
+      opacity:1,
+      delay:2
+    })
+  }, []);
 
-export default Home
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
