e9791663cfb428263ef5e4d5943444287f75c944
Umut Kütükalan <118997325+umutkutukalan@users.noreply.github.com>
2024-08-07 03:07:24 +0300

    9-video-src-update

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -8,14 +8,9 @@ const VideoCarousel = () => {
           <div key={list.id} id="slider" className="sm:pr-20 pr-10">
             <div className="video-carousel_container">
               <div className="w-full h-full bg-black rounded-3xl overflow-hidden flex-center">
-                <video
-                  src={list.video}
-                  id="video"
-                  type="video/mp4"
-                  playsInline={true}
-                  preload="auto"
-                  muted
-                ></video>
+                <video id="video" playsInline={true} preload="auto" muted>
+                  <source src={list.video} type="video/mp4" />
+                </video>
                 <div className="absolute top-12 left-[5%]">
                   {list.textLists.map((text) => (
                     <div key={text} className="font-medium md:text-2xl text-xl">
