da006a0dda2f67d9d977bd88448bdf820574637a
Umut Kütükalan <118997325+umutkutukalan@users.noreply.github.com>
2024-08-07 03:30:53 +0300

    10-startPlay-isPlaying-setVideo

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,6 +1,58 @@
+import { useEffect, useRef, useState } from "react";
 import { hightlightsSlides } from "../constants";
-
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+import gsap from "gsap";
 const VideoCarousel = () => {
+  // videoRefs(videoRef, SpanRef, DivRef)
+
+  const videoRef = useRef([]);
+
+  // video and setVideo
+  const [video, setvideo] = useState({
+    videoId: 0,
+    startPlay: false,
+    isPlaying: false,
+    isEnd: false,
+    isLastVideo: false,
+  });
+  const { videoId, startPlay, isPlaying, isEnd, isLastVideo } = video;
+
+  //LoadedData and seLoadedData (array)
+  const [loadedData, setloadedData] = useState([]);
+
+  // GSAP (#video)
+  useGSAP(() => {
+    gsap.to("#video", {
+      scrollTrigger: {
+        trigger: "#video",
+        toggleActions: "restart none none none",
+      },
+      onComplete: () => {
+        setvideo((prev) => ({
+          ...prev,
+          startPlay: true,
+          isPlaying: true,
+        }));
+      },
+    });
+  }, [videoId, isEnd]);
+
+  // loadedData | videoPause-Play
+  useEffect(() => {
+    if (loadedData.length > 3) {
+      if (!isPlaying) {
+        videoRef.current[videoId].pause();
+      } else {
+        startPlay && videoRef.current[videoId].play();
+      }
+    }
+  }, [videoId, startPlay, isPlaying, loadedData]);
+
+  const handleLoadedMetaData = (i, e) => setloadedData((prev) => [...prev, e]);
+
+  
   return (
     <>
       <div className="flex align-items">
@@ -8,7 +60,20 @@ const VideoCarousel = () => {
           <div key={list.id} id="slider" className="sm:pr-20 pr-10">
             <div className="video-carousel_container">
               <div className="w-full h-full bg-black rounded-3xl overflow-hidden flex-center">
-                <video id="video" playsInline={true} preload="auto" muted>
+                <video
+                  id="video"
+                  playsInline={true}
+                  preload="auto"
+                  muted
+                  ref={(el) => (videoRef.current[i] = el)}
+                  onPlay={() =>
+                    setvideo((prev) => ({
+                      ...prev,
+                      isPlaying: true,
+                    }))
+                  }
+                  onLoadedMetadata={(e) => handleLoadedMetaData(i, e)}
+                >
                   <source src={list.video} type="video/mp4" />
                 </video>
                 <div className="absolute top-12 left-[5%]">
